389fdf3e69850f5567bd3308767efa3923825f20
Martin Roa Villescas <m.roa.villescas@tue.nl>
2019-08-26 11:54:18 +0200

    Create a hello world plugin that calls a Julia shared lib

---
+++ examples/00-gain-mrv/run.sh
@@ -0,0 +1,3 @@
+export LD_LIBRARY_PATH=/home/mroavi/julia/usr/lib:/home/mroavi/repos/TinyB/submodules/openMHA/external_libs/julia-lib-test
+export MHA_LIBRARY_PATH=/home/mroavi/repos/TinyB/submodules/openMHA/lib;/home/mroavi/repos/TinyB/submodules/openMHA/bin
+../../bin/mha ?read:gain_live.cfg cmd=start
---
+++ mha/plugins/example1-mrv/example1-mrv.cpp
@@ -0,0 +1,170 @@
+// This file is part of the HörTech Open Master Hearing Aid (openMHA)
+// Copyright © 2004 2007 2009 2010 2012 2013 2014 2015 2017 2018 HörTech gGmbH
+//
+// openMHA is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, version 3 of the License.
+//
+// openMHA is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License, version 3 for more details.
+//
+// You should have received a copy of the GNU Affero General Public License, 
+// version 3 along with openMHA.  If not, see <http://www.gnu.org/licenses/>.
+
+
+#define JULIA_ENABLE_THREADING
+
+// Julia headers (for initialization and gc commands)
+#include "uv.h"
+#include "julia.h"
+
+//#ifdef JULIA_DEFINE_FAST_TLS // only available in Julia v0.7 and above
+//JULIA_DEFINE_FAST_TLS()
+//#endif
+
+
+/*
+ * The simplest example of an \mha plugin.
+ *
+ * This plugin scales one channel of the input signal, working in the
+ * time domain.
+ */
+
+#include "mha_plugin.hh"
+#include <iostream>
+#include <dlfcn.h>
+#include <string.h>
+
+using namespace std;
+
+extern "C" {
+
+extern int foo(int);
+extern int bar(int);
+extern int baz(int);
+
+//extern void jl_init_with_image__threading(void *, char *);
+extern void jl_atexit_hook(int);
+
+// Declare C prototype of a function defined in Julia
+extern int julia_main();
+}
+
+/** This C++ class implements the simplest example plugin for the
+ * step-by-step tutorial.  It inherits from MHAPlugin::plugin_t for
+ * correct integration in the configuration language interface.  */
+class example1_t : public MHAPlugin::plugin_t<int> {
+public:
+    /** Do-nothing constructor.  The constructor has to take these three
+     * arguments, but it does not have to use them. However, the base
+     * class has to be initialized. */
+    example1_t(algo_comm_t &ac,
+               const std::string &chain_name,
+               const std::string &algo_name)
+            : MHAPlugin::plugin_t<int>("", ac) {/* Do nothing in constructor */}
+
+    /** Release may be empty */
+    void release(void) {/* Do nothing in release */}
+
+    /** Plugin preparation. This plugin checks that the input signal has the
+     * waveform domain and contains at least one channel
+     * @param signal_info
+     *   Structure containing a description of the form of the signal (domain,
+     *   number of channels, frames per block, sampling rate.
+     */
+    void prepare(mhaconfig_t &signal_info) {
+
+        string libmrv_path = "/home/mroavi/repos/TinyB/submodules/openMHA/external_libs/julia-lib-test/libmrv.so";
+
+        cout << "Hello, World!\n";
+
+        void *handle = dlopen(libmrv_path.c_str(), RTLD_GLOBAL | RTLD_NOW);
+        if (!handle) {
+            /* Failed to load the library */
+            throw MHA_Error(__FILE__, __LINE__, dlerror());
+        }
+        dlerror();
+        // int dlclose(void *handle); // where should I run dlclose?
+
+        // First argument is the usr/bin directory where the julia binary is, or NULL to guess.
+        // Second argument is the path of a system image file (*.ji) relative to the
+        // first argument path, or relative to the default julia home dir.
+        // The default is something like ../lib/julia/sys.ji
+        jl_init_with_image__threading(NULL, (char*)libmrv_path.c_str());
+
+        cout << foo(1) << '\n';
+        cout << bar(1) << '\n';
+        cout << baz(1) << '\n';
+
+        // call the work function, and get back a value
+        julia_main();
+
+        jl_atexit_hook(0);
+
+        if (signal_info.domain != MHA_WAVEFORM)
+            throw MHA_Error(__FILE__, __LINE__,
+                            "This plugin can only process waveform signals.");
+        if (signal_info.channels < 1)
+            throw MHA_Error(__FILE__, __LINE__,
+                            "This plugin requires at least one input channel.");
+    }
+
+    /** Signal processing performed by the plugin.
+     * This plugin multiplies the signal in the first audio channel by
+     * a factor 0.1.
+     * @param signal
+     *   Pointer to the input signal structure.
+     * @return
+     *   Returns a pointer to the input signal structure,
+     *   with a the signal modified by this plugin.
+     *   (In-place processing)
+     */
+    mha_wave_t *process(mha_wave_t *signal) {
+        unsigned int channel = 1; // channels and frames counting starts with 0
+        float factor = 0.1f;
+        unsigned int frame;
+
+        // Scale channel number "channel" by "factor":
+        for (frame = 0; frame < signal->num_frames; frame++) {
+            // Waveform channels are stored interleaved.
+            signal->buf[signal->num_channels * frame + channel] *= factor;
+        }
+        // Algorithms may process data in-place and return the input signal
+        // structure as their output signal:
+        return signal;
+    }
+};
+
+/*
+ * This macro connects the example1_t class with the \mha plugin C interface
+ * The first argument is the class name, the other arguments define the 
+ * input and output domain of the algorithm.
+ */
+MHAPLUGIN_CALLBACKS(example1, example1_t, wave, wave
+)
+
+/*
+ * This macro creates code classification of the plugin and for
+ * automatic documentation.
+ *
+ * The first argument to the macro is a space separated list of
+ * categories, starting with the most relevant category. The second
+ * argument is a LaTeX-compatible character array with some detailed
+ * documentation of the plugin.
+ */
+MHAPLUGIN_DOCUMENTATION\
+(example1,
+"example level-modification audio-channels",
+"The {\\bf simplest} example of an \\mha{} plugin.\n\n"
+"This plugin scales one channel of the input signal, working in the "
+"time domain."
+)
+
+// Local Variables:
+// compile-command: "make"
+// c-basic-offset: 4
+// indent-tabs-mode: nil
+// coding: utf-8-unix
+// End:
