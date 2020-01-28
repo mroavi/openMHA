e4e3fded8221b8adb00c3cf950862b33ac76c085
Martin Roa Villescas <m.roa.villescas@tue.nl>
2020-01-28 15:29:53 +0100

    Activate the julia project before building the shared library

--- external_libs/package-compiler/build-libs.jl
+++ external_libs/package-compiler/build-libs.jl
@@ -1,6 +1,8 @@
 # This is an example of how to use PackageCompiler.jl to generate a shared lib.
 # The resulting library files can be called from a C++ program.
 
+using Pkg
+
 # ------------------------------------------------------------------------------
 # Build the shared library
 # ------------------------------------------------------------------------------
@@ -9,6 +11,9 @@ output_dir =        joinpath(@__DIR__, "../x86_64-linux-gcc-7/lib")
 pkg_compiler_dir =  joinpath(@__DIR__, "../../../PackageCompiler.jl")
 lib_src_dir =       joinpath(@__DIR__, ".")
 julia_lib_dir = "/home/mroavi/julia/usr/lib"
+
+# Activate PackageCompiler project
+Pkg.activate(pkg_compiler_dir)
 
 # Build the shared library
 run(`julia --project=$(pkg_compiler_dir)/Project.toml $(pkg_compiler_dir)/juliac.jl -vas -g2 -d $(output_dir) $(lib_src_dir)/libmrv.jl`)
