12f14c38d068427ce9850da5fd985cf1025bd7da
Martin Roa Villescas <m.roa.villescas@tue.nl>
2020-02-29 00:11:15 +0100

    Parse architecture from CFLAGS env variable

--- external_libs/package-compiler/build-libs.jl
+++ external_libs/package-compiler/build-libs.jl
@@ -1,13 +1,22 @@
 # This is an example of how to use PackageCompiler.jl to generate a shared lib.
 # The resulting library files can be called from a C++ program.
 
+# IMPORTANT: this file must be invoked by openMHA's makefile
+
 using Pkg
 
 # ------------------------------------------------------------------------------
 # Build the shared library
 # ------------------------------------------------------------------------------
 
-output_dir =        joinpath(@__DIR__, "../x86_64-linux-gcc-7/lib")
+# Get the CFLAGS environment variable
+cflags = get(ENV, "CFLAGS", -1)
+# Get the include path (last entry) which happens to contain the architecture
+include_dir = split(cflags, " ")[end]
+# Extract the architecture from the include path
+arch = splitdir(splitdir(include_dir)[end-1])[end]
+
+output_dir =        joinpath(@__DIR__, "..", arch, "lib")
 pkg_compiler_dir =  joinpath(@__DIR__, "../../../PackageCompiler.jl")
 lib_src_dir =       joinpath(@__DIR__, ".")
 julia_lib_dir = "/home/mroavi/julia/usr/lib"
